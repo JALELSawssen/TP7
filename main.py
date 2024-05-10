ed9fe046f5c16a33b2c1add3641cc813dc429a12
Ahmed <ahmed@tek-up.tn>
2024-05-10 15:28:28 +0100

    [feature_mul]__MAJ main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def add(x,y):
     return x+y
 
+def mul(x,y):
+    return x*y
 
-a=int(input("Donnez x: "))
-b=int(input("Donnez y: "))
+a=int(input("Donnez a: "))
+b=int(input("Donnez b: "))
 print("la somme est", add(a,b))
+print("la multiplication de",a," par ",b," vaut", mul(a,b))
